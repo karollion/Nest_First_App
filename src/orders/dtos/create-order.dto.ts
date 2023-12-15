0a66312bf755e850ad98ec1bf837b435fd791d5e
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-15 19:21:38 +0100

    Add to orders methods: deleteById, create, update

---
+++ src/orders/dtos/create-order.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateOrderDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(3, 30)
+  client: string;
+
+  @IsNotEmpty()
+  @IsString()
+  productId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 100)
+  address: string;
+}
